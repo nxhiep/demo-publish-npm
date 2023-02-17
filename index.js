32dde0c2e837c09d437031627ebc6af643e28fa4
Hiep Nguyen <nxhiep.1996@gmail.com>
2023-02-17 11:02:36 +0700

    init project

---
+++ index.js
@@ -0,0 +1,17 @@
+const moment = require("moment");
+
+const formatDate = (date) => {
+    return moment(date).format("dd/MM/yyyy");
+}
+
+const isValidateEmail = (email) => {
+    return !!email && !!String(email).toLowerCase().match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+}
+
+
+module.exports = {
+    formatDate,
+    isValidateEmail
+}
